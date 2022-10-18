1c8e4f256426b0f044ed2e4a3bf4452457231c09
sureshnanjan <sureshnanjan@outlook.com>
2022-10-18 22:44:28 +0530

    Initial Repository for Training

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+// Write a message to the console.
+let myName = "Suresh";
+const FIXEDVALUE = 10000;
+console.log('hello world!');
+function SayHello(){
+    console.log("Hello JavaScript in VsCode");
+}
+
+SayHello();
