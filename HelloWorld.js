2acb36821f753266c0fc14fd28522f18d7a316a0
sureshnanjan <sureshnanjan@outlook.com>
2022-10-19 05:05:47 +0530

    Adding the new Changes for Object JS

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,7 +3,8 @@ let myName = "Suresh";
 const FIXEDVALUE = 10000;
 console.log('hello world!');
 function SayHello(){
-    console.log("Hello JavaScript in VsCode");
+    console.log("Hello Hello JavaScript in VsCode");
 }
-
 SayHello();
+
+
---
+++ Object.js
@@ -0,0 +1,3 @@
+// Object Literal
+
+const myObject = {};
